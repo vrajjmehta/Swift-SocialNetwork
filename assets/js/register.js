f541abb450b551d3a91b4bd58ec087c419e34c67
Vraj Mehta <vrajjmehta@gmail.com>
2019-10-09 00:06:33 +1100

    LoginPage with js

--- assets/js/register.js
+++ assets/js/register.js
@@ -1,8 +1,17 @@
 $(document).ready(function(){
 
-    //on click signup hider login and show registeration form
+    //on click signup hide login and show registeration form
     $("#signup").click(function(){
-        
-    })
+        $("#first").slideUp("slow",function(){
+            $("#second").slideDown("slow");
+        });
+    });
+
+    //on click signup hide registeration and show login
+    $("#signin").click(function(){
+        $("#second").slideUp("slow",function(){
+            $("#first").slideDown("slow");
+        });
+    });
 
 });
--- register.php
+++ register.php
@@ -12,6 +12,22 @@ require 'includes/form_handlers/login_handler.php';
     <script src="assets/js/register.js"></script>
 </head>
 <body>
+    <?php
+    
+    if(isset($_POST['register_button'])){
+        echo '
+        <script>
+
+        $(document).ready(function(){
+            $("#first").hide();
+            $("#second").show();
+        });
+
+        </script>
+        ';
+    }
+    ?>
+
     <div class="wrapper">
         
         <div class="login_box">
