9843c6f195f3212287566f79583e18670c90e617
Vraj Mehta <vrajjmehta@gmail.com>
2019-10-19 15:23:24 +1100

    font awesome

--- includes/header.php
+++ includes/header.php
@@ -3,6 +3,8 @@ require 'config/config.php';
 
 if(isset($_SESSION['username'])){
     $userLoggedIn = $_SESSION['username'];
+    $user_details_query = mysqli_query($con,"SELECT * FROM users WHERE username='$userLoggedIn'");
+    $user = mysqli_fetch_array($user_details_query);
 }
 else{
     header("Location: register.php");
@@ -16,6 +18,7 @@ else{
     <script src="assets/js/bootstrap.js"></script>
 
      <!-- CSS -->
+    <link rel="stylesheet" href="font-awesome-4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.css">
     <link rel="stylesheet" type="text/css" href="assets/css/style.css">
 </head>
@@ -26,10 +29,27 @@ else{
             <a href="index.php"> Swift Social Media </a>
         </div>
 
+    <nav>
+        <a href="#">
+            <?php echo $user['first_name']; ?>
+        </a>
+        <a href="#">
+            <i class="fa fa-home" aria-hidden="true"></i>
+        </a>
+        <a href="#">
+            <i class="fa fa-envelope" aria-hidden="true"></i>
+        </a>
+        <a href="#">
+            <i class="fa fa-bell-o" aria-hidden="true"></i>
+        </a>
+        <a href="#">
+            <i class="fa fa-users" aria-hidden="true"></i>
+        </a>
+        <a href="#">
+            <i class="fa fa-cog" aria-hidden="true"></i>
+        </a>
+    </nav>
+
     </div>
 
-    <nav>
-        <a href="#">Home</a>
-        <a href="#">messages</a>
-        <a href="#">settings</a>
-    </nav>
+    <div class="wrapper">
--- index.php
+++ index.php
@@ -1,6 +1,19 @@
 <?php 
 include("includes/header.php");
 ?>
-Hello
+    <div class="user_details column">
+        <a href="#">
+            <img src="<?php echo $user['profile_pic'];  ?>">
+        </a>
+    </div>
+
+
+
+
+
+
+
+
+</div>
 </body>
 </html> 
